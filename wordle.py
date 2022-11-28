45e2135af0cdbf6e690b627162b0258d093e0cad
kailash-unc <kailash@email.unc.edu>
2022-11-27 19:22:37 -0500

    try catch, loop functionality, error handling

--- wordle.py
+++ wordle.py
@@ -9,11 +9,18 @@ def get_word(i):
     js = requests.get(url=url).text
     
     output = json.loads(js.split(',it=')[1].split(';var lt=')[0])
-    print(type(output))
+    
     known = [output.index('happy'), datetime.datetime.strptime('Nov 27 2022', '%b %d %Y')]
     
     current_index = known[0] + (datetime.datetime.now() - known[1]).days
     
-    print('The wordle answer is "' + output[current_index + int(i)] + '"')
+    print('\nThe wordle answer is "' + output[current_index + int(i)] + '"\n')
 
-get_word(i = input())
+while(True):
+    try:
+        print('\nPlease enter a valid integer corresponding to a day')
+        get_word(i = input('Ex: [1 = Tommorow, 0 = Today, -1 = Yesterday] \n\nEnter Number: '))
+        break
+    except:
+        print('\n Error: Invalid integer')
+       
